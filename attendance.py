bf58b441d40de3eb318f48d03fee6adc0bef5f34
Arjun Narayan <arjun@hubert.org>
2018-06-01 21:59:44 +0530

    added comments and a readme

--- attendance.py
+++ attendance.py
@@ -1,27 +1,32 @@
 import redis
-import struct
 import random
+
 
 def set_present(r, day, id):
     r.setbit(day, id, True)
 
+#Can be used to query for no of users instantly
 def get_present_for_day_so_far(r, day):
     return r.bitcount(day)
 
+#Initializes all attendance to false
 def initialze_database(r, days):
     for day in days:
         for i in range(100):
             r.setbit(day, i, False)
 
+#Generates keys which are used as keys on redis itself
 def generate_key(day):
     return "day" + str(day)
 
+#Populates with random attendances
 def populate_with_random_data(r, keys):
     for key in keys:
         for i in range(100):
             if random.choice([True, False]):
                 set_present(r, key, i)
 
+#Gets all present for a particular day
 def present_on_day(day):
     present_ids = []
     for i in range(12):
@@ -34,9 +39,9 @@ def present_on_day(day):
     for shift in range(5):
         if((final >> shift) & 1) == 1:
             present_ids.append(96 + shift)
-
     return len(present_ids) , sorted(present_ids)
 
+#Gets all absent for a particular day
 def absent_on_day(day):
     count = 0
     absent_ids = []
@@ -54,6 +59,7 @@ def absent_on_day(day):
 
     return len(absent_ids) , sorted(absent_ids)
 
+#Gets all present for any two given days
 def present_both_days(day1, day2):
     present_ids = []
     for i in range(12):
@@ -70,6 +76,7 @@ def present_both_days(day1, day2):
 
     return len(present_ids), present_ids
 
+#Gets all absent for any two given days
 def absent_both_days(day1, day2):
     absent_ids = []
     for i in range(12):
@@ -86,13 +93,16 @@ def absent_both_days(day1, day2):
 
     return len(absent_ids), absent_ids
 
-
+#Problem solution generates random values and prints attendance for each day and consecitive days.
 def problem(r, no_of_days):
     keys = [generate_key(i + 1) for i in range(no_of_days)]
     print("Initializing database.....")
     initialze_database(r, keys)
     print("Populating Random Values.........")
     populate_with_random_data(r, keys)
+
+    #This is done to limit the number of calls to the database else calls have to made in each function
+    #All the functions are just passed the byte arrays
     days = [bytearray(r.get(key)) for key in keys]
     print("==============PER DAY===============")
     for index , key in enumerate(keys):
---
+++ readme.md
@@ -0,0 +1,8 @@
+#Problem 1
+Solved as per my understanding of the given statement.
+
+##Install dependencies
+> pip3 install -r requirements.txt
+
+##Run
+> python3 attendance.py
